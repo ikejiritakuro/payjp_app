46e010e9d4312192ec827e08f0b91efb1866e25b
ikejiritakuro <talangchikao165@gmail.com>
2021-07-20 19:15:26 +0900

    商品一覧機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'items/index'
   devise_for :users
+  root 'items#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.create(name: 'ペン', price: 100)
+Item.create(name: 'ペン2', price: 200)
+Item.create(name: 'ノート1', price: 400)
+Item.create(name: 'ノート2', price: 600)
+Item.create(name: 'ペンケース1', price: 800)
+Item.create(name: 'ペンケース', price: 1000)
+Item.create(name: '本1', price: 1500)
+Item.create(name: '本2', price: 2000)
+Item.create(name: 'メガネ1', price: 20000)
+Item.create(name: 'メガネ2', price: 35000)
