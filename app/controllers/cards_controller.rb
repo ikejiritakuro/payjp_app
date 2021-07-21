a1bf07a7faec26829e489836a1c561f91d75536c
ikejiritakuro <talangchikao165@gmail.com>
2021-07-21 16:52:09 +0900

    カード登録の実装

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,22 @@
+class CardsController < ApplicationController
+  def new
+  end
+
+  def create
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer = Payjp::Customer.create(
+      description: 'test',
+      card: params[:card_token]
+    )
+    card = Card.new(
+      card_token: params[:card_token],
+      customer_token: customer.id,
+      user_id: current_user.id
+    )
+    if card.save
+      redirect_to root_path
+    else
+      redirect_to action: "new"
+    end
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def show
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    card = Card.find_by(user_id: current_user.id)
+
+    redirect_to new_card_path and return unless card.present?
+
+    customer = Payjp::Customer.retrieve(card.customer_token) 
+    @card = customer.cards.first
   end
 
   def update
